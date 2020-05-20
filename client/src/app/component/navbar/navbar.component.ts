f9b6499492b2979a01d7cf0b50c661a498d78ad9
Alexandre-1Briere <alexandrebriere@live.ca>
2020-05-20 09:25:43 -0400

    adding a menu-item array to the nav bar

--- client/src/app/component/navbar/navbar.component.ts
+++ client/src/app/component/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MenuItem} from '../../interface/menu-item';
 
 @Component({
   selector: 'app-navbar',
@@ -6,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  itemList: MenuItem[];
+  constructor() {
+    this.itemList = [];
+  }
 
   ngOnInit() {
   }
