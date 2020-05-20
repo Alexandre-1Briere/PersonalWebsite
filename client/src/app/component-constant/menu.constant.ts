b3e6f4770ff9979efe96a5c6e67166334558d2d6
Alexandre-1Briere <alexandrebriere@live.ca>
2020-05-20 09:28:12 -0400

    adding the menu file to init the menu-item of the navbar

---
+++ client/src/app/component-constant/menu.constant.ts
@@ -0,0 +1,14 @@
+import {MenuItem} from '../interface/menu-item';
+
+export let menuFR: MenuItem[] = [
+  {
+    name: 'Acceuil',
+    path: '/',
+    isActive: true,
+  },
+  {
+    name: 'Projets',
+    path: '/Projets',
+    isActive: true,
+  }
+];
